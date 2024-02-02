09b736d50f9f18b56d1be38488040ddd4e67ff99
perompak_kode <iqbaltech29@gmail.com>
2024-02-02 11:13:14 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+// import 'package:flutter/material.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+
+//   final String title;
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   static const String counterNumberPrefs = 'counterNumber';
+//   int _counter = 0;
+
+//   void _saveNumber() async {
+//     final prefs = await SharedPreferences.getInstance();
+//     prefs.setInt(counterNumberPrefs, _counter);
+//   }
+
+//   void _resetNumber() async {
+//     final prefs = await SharedPreferences.getInstance();
+//     prefs.remove(counterNumberPrefs);
+//     _loadNumber();
+//   }
+
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//       _saveNumber();
+//     });
+//   }
+
+//   void _loadNumber() async {
+//     final prefs = await SharedPreferences.getInstance();
+//     setState(() {
+//       _counter = prefs.getInt(counterNumberPrefs) ?? 0;
+//     });
+//   }
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _loadNumber();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headlineMedium,
+//             ),
+//             ElevatedButton(
+//               onPressed: _resetNumber,
+//               child: const Text("Reset"),
+//             )
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'db_provider.dart';
+import 'note_list_page.dart';
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => DbProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const NoteListPage(),
+      ),
+    );
+  }
+}
